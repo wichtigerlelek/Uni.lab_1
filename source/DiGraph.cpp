61b4e66d1810c37228015650e38bab8bba7e87bf
Felix Garcia <felixgarciaorg@gmail.com>
2025-11-15 13:00:33 +0100

    Aufgabe 3

--- source/DiGraph.cpp
+++ source/DiGraph.cpp
@@ -9,24 +9,54 @@ DiGraph::DiGraph() {
 	this->next_free_node_id = 0;
 }
 
-DiGraph::~DiGraph() {}
-
-std::int32_t DiGraph::get_node_count() const { return 0; }
-
-Node* DiGraph::add_node() {
-	Node new_node = Node(this->next_free_node_id);
-	this->node_map[this->next_free_node_id] = &new_node;
-	this->next_free_node_id++;
-	return &new_node;
+DiGraph::~DiGraph() {
+	for (auto &entry : this->node_map)
+	{
+		delete entry.second;
+	}
+	this->node_map.clear(); // Deletes all elements in the map (probably not even nessesary)
 }
 
-Node* DiGraph::add_node_carelessly(std::int32_t new_node_id) { return nullptr; }
+std::int32_t DiGraph::get_node_count() const { 
+	return this->node_map.size();
+}
 
-void DiGraph::set_next_free_node_id(std::int32_t value) {}
+Node* DiGraph::add_node()
+{
+	Node* new_node = new Node(this->next_free_node_id);
+	this->node_map[this->next_free_node_id] = new_node;
+	this->next_free_node_id++;
+	return new_node;
+}
 
-Node* DiGraph::get_node_by_id(std::int32_t node_id) const { return nullptr; }
+Node* DiGraph::add_node_carelessly(int32_t new_node_id)
+{
+	Node* new_node = new Node(new_node_id);
+	this->node_map[new_node_id] = new_node;
+	return new_node;
+}
 
-void DiGraph::add_edge(int32_t source_id, int32_t target_id) {}
+void DiGraph::set_next_free_node_id(std::int32_t value) {
+	this->next_free_node_id = value;
+}
+
+Node* DiGraph::get_node_by_id(std::int32_t node_id) const {
+	Node* node = this->node_map.at(node_id);
+	return node;
+}
+
+void DiGraph::add_edge(int32_t source_id, int32_t target_id) {
+	if (node_map.find(source_id) == node_map.end() ||
+		node_map.find(target_id) == node_map.end())
+		throw std::invalid_argument("ERROR: Both Nodes id's must be in the source map");
+
+	Node* start_node = DiGraph::get_node_by_id(source_id);
+	Node* target_node = DiGraph::get_node_by_id(target_id);
+
+	start_node->add_out_edge(target_node);
+	target_node->add_in_edge(start_node);
+
+}
 
 void DiGraph::export_to_dot_file(std::string file_path) const {}
 
